4b2ef5675a84cbc1d7487a7c035c1f7897d9969f
farrelputras <fernandiofarrel@gmail.com>
2025-07-14 23:16:26 +0700

    feat: hide servis resource

--- app/Filament/Resources/ServisResource.php
+++ app/Filament/Resources/ServisResource.php
@@ -15,6 +15,8 @@ use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class ServisResource extends Resource
 {
+    protected static bool $shouldRegisterNavigation = false;
+
     protected static ?string $model = Servis::class;
 
     protected static ?string $navigationIcon = 'fas-gear';
